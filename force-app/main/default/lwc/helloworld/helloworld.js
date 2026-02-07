8242e7ee12729b58809d0bbfe857d14358b27295
Tariq <tariq.riley-smith@bluewavegroup.eu>
2026-02-07 13:01:48 +0000

    Clean up

--- force-app/main/default/lwc/helloworld/helloworld.js
+++ force-app/main/default/lwc/helloworld/helloworld.js
@@ -7,40 +7,31 @@ export default class HelloWorld extends LightningElement {
     //recordLink;
     
     columns = [
-        { 
-          label: 'Name', fieldName: 'recordLink', type: 'url',
-            typeAttributes: {
-               label: { fieldName: 'Name' }, 
-               target: '_blank'
-            }
+        { label: 'Name', fieldName: 'recordLink', type: 'url',
+            typeAttributes: {label: { fieldName: 'Name' }, target: '_blank'}
         },
         { label: 'Email', fieldName: 'Email' },
-        { label: 'Phone', fieldName: 'Phone' },
-        { label: 'Update Calendar', type: 'button',         
-            typeAttributes: {
-            label: 'Select',
-            name: 'select_contact',
-            variant: 'brand'
+        { label: 'Appointment Date', fieldName: 'AppointmentDate__c' },
+        { label: 'Change Appointment Date', type: 'url', fieldName: 'flowLink',         
+            typeAttributes: {label: 'Update', target: '_blank'}
         }
-
-}
     ];
 
     handleChange(event){
         this.searchKey = event.target.value;
     }
 
-     @wire(getContacts, { searchText : '$searchKey'  }) 
+    @wire(getContacts, { searchText : '$searchKey'  }) 
      wiredContacts({error, data}) {
-
         if(data){
-                //this.contacts = data;
-                this.contacts = data.map(c => {
-                return {
-                    ...c,
-                    recordLink: '/' + c.Id
-                };
-            });
+        this.contacts = data.map(c => {
+            return {
+                ...c,
+                recordLink: '/' + c.Id,
+                flowLink: '/flow/Contact_Update_Contact_Appointment_Date?recordId=' + c.Id
+
+        };
+    });
 
         }
         else if (error){
