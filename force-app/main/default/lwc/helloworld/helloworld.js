f9b18006466476fb19178468be5227b450858e5f
Tariq <tariq.riley-smith@bluewavegroup.eu>
2026-02-07 14:58:44 +0000

    css and modal enhancements

--- force-app/main/default/lwc/helloworld/helloworld.js
+++ force-app/main/default/lwc/helloworld/helloworld.js
@@ -1,50 +1,100 @@
-import { LightningElement, track, wire} from 'lwc';
-import getContacts from '@salesforce/apex/ContactController.getContacts';
-export default class HelloWorld extends LightningElement {
-    @track searchKey = '';
-    @track contacts = [];
-    @track selectedContacts = [];
-    //recordLink;
-    
-    columns = [
-        { label: 'Name', fieldName: 'recordLink', type: 'url',
-            typeAttributes: {label: { fieldName: 'Name' }, target: '_blank'}
-        },
-        { label: 'Email', fieldName: 'Email' },
-        { label: 'Appointment Date', fieldName: 'AppointmentDate__c' },
-        { label: 'Change Appointment Date', type: 'url', fieldName: 'flowLink',         
-            typeAttributes: {label: 'Update', target: '_blank'}
-        }
-    ];
-
-    handleChange(event){
-        this.searchKey = event.target.value;
-    }
-
-    @wire(getContacts, { searchText : '$searchKey'  }) 
-     wiredContacts({error, data}) {
-        if(data){
-        this.contacts = data.map(c => {
-            return {
-                ...c,
-                recordLink: '/' + c.Id,
-                flowLink: '/flow/Contact_Update_Contact_Appointment_Date?recordId=' + c.Id
-
-        };
-    });
-
-        }
-        else if (error){
-                this.contacts = undefined;
-                console.error(error);
-        }
-     }
-
-     
-    handleRowSelection(event) {
-        this.selectedContacts = event.detail.selectedRows;
-        console.log('Selected:', JSON.stringify(this.selectedContacts));
-
-    }
-
+import {
+  LightningElement,
+  track,
+  wire
 }
+from 'lwc';
+import getContacts from '@salesforce/apex/ContactController.getContacts';
+export
+default class HelloWorld extends LightningElement {@track searchKey = '';@track contacts = [];@track selectedContacts = [];
+    //recordLink;
+    @track showFlowModal = false;@track flowInputVariables = [];
+
+    columns = [
+    {
+      label: 'Name',
+      fieldName: 'recordLink',
+      type: 'url',
+      typeAttributes: {
+        label: {
+          fieldName: 'Name'
+        },
+        target: '_blank'
+      }
+    },
+    {
+      label: 'Email',
+      fieldName: 'Email'
+    },
+    {
+      label: 'Appointment Date',
+      fieldName: 'AppointmentDate__c'
+    },
+    {
+      type: 'button',
+      typeAttributes: {
+        label: 'Update',
+        name: 'open_modal',
+        variant: 'brand'
+      }
+    }];
+
+    handleChange(event) {
+      this.searchKey = event.target.value;
+    }
+
+    @wire(getContacts, {
+      searchText: '$searchKey'
+    }) wiredContacts({
+      error,
+      data
+    }) {
+      if (data) {
+        this.contacts = data.map(c => {
+          return {...c,
+            recordLink: '/' + c.Id,
+            flowLink: '/flow/Contact_Update_Contact_Appointment_Date?recordId=' + c.Id
+          };
+        });
+
+      } else if (error) {
+        this.contacts = undefined;
+        console.error(error);
+      }
+    }
+
+    handleRowAction(event) {
+      const action = event.detail.action.name;
+      const row = event.detail.row;
+
+      if (action === 'open_modal') {
+        this.openFlowModal(row.Id); // ← your modal function
+      }
+    }
+    
+    openFlowModal(contactId) {
+      this.flowInputVariables = [{
+        name: 'recordId',
+        // must match your Flow input variable
+        type: 'String',
+        value: contactId // hard-code a Contact Id just to test
+      }];
+      this.showFlowModal = true;
+    }
+
+    closeFlowModal() {
+      this.showFlowModal = false;
+    }
+
+    handleFlowStatusChange(event) {
+      const status = event.detail.status;
+      if (status === 'FINISHED' || status === 'FINISHED_SCREEN') {
+        this.showFlowModal = false;
+        window.location.reload();
+      } else if (status === 'ERROR') {
+        this.showFlowModal = false;
+        console.error(event.detail.errors);
+      }
+    }
+
+  }
