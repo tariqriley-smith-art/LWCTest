cca2969200363ac3ac45c1c8a34e7fa806f2b537
Tariq <tariq.riley-smith@bluewavegroup.eu>
2026-02-08 13:17:06 +0000

    Improvements

--- force-app/main/default/lwc/contactAbsenses/contactAbsenses.js
+++ force-app/main/default/lwc/contactAbsenses/contactAbsenses.js
@@ -3,15 +3,33 @@ import getAbsenses from '@salesforce/apex/ContactController.getAbsenses';
 export default class ContactAbsenses extends LightningElement {
     @track absenses = [];
     @track error;
+    @track selectedValue = 'today';
 
-    @wire(getAbsenses)
+    @wire(getAbsenses, {dateFilter: '$selectedValue'})
     wiredAbsenses({ data, error }) {
-        if (data) {
-            this.absenses = data;
-            this.error = undefined;
-        } else if (error) {
+    if (data) {
+        this.selectedValue = this.selectedValue;
+        this.absenses = data.map(a => ({
+            ...a,
+            absenseLink: '/' + a.Id,
+            contactName: a.Contact__r.Name
+        }));
+    }
+    else if (error) {
             this.error = error;
             this.absenses = [];
         }
+      }
+
+    get options() {
+        return [
+            { label: 'Today', value: 'today' },
+            { label: 'This Week', value: 'this_week' },
+            { label: 'This Month', value: 'this_month' }
+        ];
+    }
+
+    handleFilterChange(event) {
+        this.selectedValue = event.detail.value;
     }
 }
--- force-app/main/default/lwc/helloworld/helloworld.js
+++ force-app/main/default/lwc/helloworld/helloworld.js
@@ -1,124 +1,123 @@
-import {LightningElement, track, wire} from 'lwc';
 import getContacts from '@salesforce/apex/ContactController.getContacts';
-import sendEmailToContact from '@salesforce/apex/ContactController.getContacts';
-import { NavigationMixin } from 'lightning/navigation';
-export
-default class HelloWorld extends NavigationMixin(LightningElement) {
-    @track searchKey = '';
-    @track contacts = [];
-    @track selectedContacts = [];
-    //recordLink;
-    @track showFlowModal = false;
-    @track flowInputVariables = [];
+import {NavigationMixin} from 'lightning/navigation';
+import {LightningElement, track, wire} from 'lwc';
 
-    columns = [
+export default class HelloWorld extends NavigationMixin
+(LightningElement) {
+  @track searchKey = '';
+  @track contacts = [];
+  @track selectedContacts = [];
+  @track showFlowModal = false;
+  @track flowInputVariables = [];
+  @track selectedContactId;
+
+  columns = [
     {
       label: 'Name',
       fieldName: 'recordLink',
       type: 'url',
-      typeAttributes: {
-        label: {
-          fieldName: 'Name'
-        },
-        target: '_blank'
-      }
+      typeAttributes: {label: {fieldName: 'Name'}, target: '_blank'}
     },
-    {
-      label: 'Email',
-      fieldName: 'Email'
-    },
-    {
-      label: 'Appointment Date',
-      fieldName: 'AppointmentDate__c'
-    },
-    {
+    {label: 'Email', fieldName: 'Email'},
+    {label: 'Appointment Date', fieldName: 'AppointmentDate__c'}, {
       type: 'button',
-      typeAttributes: {
-        label: 'Update',
-        name: 'open_modal',
-        variant: 'brand'
-      }
-    }];
-
-    handleChange(event) {
-      this.searchKey = event.target.value;
+      typeAttributes: {label: 'Update', name: 'open_modal', variant: 'brand'}
     }
+  ];
 
-    @wire(getContacts, {searchText: '$searchKey'}) 
-    wiredContacts({error, data }) {
-      if (data) {
-        this.contacts = data.map(c => {
-          return {...c,
-            recordLink: '/' + c.Id,
-            flowLink: '/flow/Contact_Update_Contact_Appointment_Date?recordId=' + c.Id
-          };
-        });
-
-      } else if (error) {
-        this.contacts = undefined;
-        console.error(error);
-      }
-    }
-
-    handleRowAction(event) {
-      const action = event.detail.action.name;
-      const row = event.detail.row;
-
-      if (action === 'open_modal') {
-        this.openFlowModal(row.Id); // ← your modal function
-      }
-    }
-
-    selectedContactId;
-
-    handleRowSelection(event) {
-        const selected = event.detail.selectedRows;  
-        this.selectedContactId = selected.length === 1 ? selected[0].Id : null;
-        console.log(this.selectedContactId);
-    }
-
-     openEmailComposer() {
-        if (!this.selectedContactId) {
-            alert('Please select a contact first.');
-            return;
-        }
-
-        else
-        this[NavigationMixin.Navigate]({
-            type: 'standard__quickAction',
-            attributes: {
-                apiName: 'Global.SendEmail'
-            },
-            state: {
-                recordId: this.selectedContactId
-            }
-        });
-    }
-
-    
-    openFlowModal(contactId) {
-      this.flowInputVariables = [{
-        name: 'recordId',
-        // must match your Flow input variable
-        type: 'String',
-        value: contactId // hard-code a Contact Id just to test
-      }];
-      this.showFlowModal = true;
-    }
-
-    closeFlowModal() {
-      this.showFlowModal = false;
-    }
-
-    handleFlowStatusChange(event) {
-      const status = event.detail.status;
-      if (status === 'FINISHED' || status === 'FINISHED_SCREEN') {
-        this.showFlowModal = false;
-        window.location.reload();
-      } else if (status === 'ERROR') {
-        this.showFlowModal = false;
-        console.error(event.detail.errors);
-      }
-    }
-
+  handleChange(event) {
+    this.searchKey = event.target.value;
   }
+
+  @wire(getContacts, {searchText: '$searchKey'})
+  wiredContacts({error, data}) {
+    if (data) {
+      this.contacts = data.map(c => {
+        return {
+          ...c,
+          recordLink: '/' + c.Id,
+          flowLink:
+              '/flow/Contact_Update_Contact_Appointment_Date?recordId=' + c.Id
+        };
+      });
+
+    } else if (error) {
+      this.contacts = undefined;
+      console.error(error);
+    }
+  }
+
+  handleRowSelection(event) {
+    const selected = event.detail.selectedRows;
+    this.selectedContactId = selected.length === 1 ? selected[0].Id : null;
+    console.log(this.selectedContactId);
+  }
+
+  openEmailComposer() {
+    if (!this.selectedContactId) {
+      alert('Please select a contact first.');
+      return;
+    }
+
+    else
+      this[NavigationMixin.Navigate]({
+        type: 'standard__quickAction',
+        attributes: {apiName: 'Global.SendEmail'},
+        state: {recordId: this.selectedContactId}
+      });
+  }
+
+  openCreateQuickAction() {
+    const contactId = this.selectedContactId;
+    console.log('contactID: ' + contactId);
+    this[NavigationMixin.Navigate]({
+      type: 'standard__objectPage',
+      attributes: {objectApiName: 'Absense__c', actionName: 'new'},
+      state: {defaultFieldValues: `Contact__c=${contactId}`}
+    });
+  }
+
+  createCase() {
+    const contactId = this.selectedContactId;
+    console.log('contactID: ' + contactId);
+    this[NavigationMixin.Navigate]({
+      type: 'standard__objectPage',
+      attributes: {objectApiName: 'Case', actionName: 'new'},
+      state: {defaultFieldValues: `ContactId=${contactId}`}
+    });
+  }
+
+  handleRowAction(event) {
+    const action = event.detail.action.name;
+    const row = event.detail.row;
+
+    if (action === 'open_modal') {
+      this.openFlowModal(row.Id);  // ← your modal function
+    }
+  }
+
+  openFlowModal(contactId) {
+    this.flowInputVariables = [{
+      name: 'recordId',
+      // must match your Flow input variable
+      type: 'String',
+      value: contactId  // hard-code a Contact Id just to test
+    }];
+    this.showFlowModal = true;
+  }
+
+  closeFlowModal() {
+    this.showFlowModal = false;
+  }
+
+  handleFlowStatusChange(event) {
+    const status = event.detail.status;
+    if (status === 'FINISHED' || status === 'FINISHED_SCREEN') {
+      this.showFlowModal = false;
+      window.location.reload();
+    } else if (status === 'ERROR') {
+      this.showFlowModal = false;
+      console.error(event.detail.errors);
+    }
+  }
+}
